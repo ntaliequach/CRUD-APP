7e928bcc7a1efab080a1391255d9dc1fddf0ab23
ntaliequach <natalie.quach03@gmail.com>
2024-01-05 01:35:45 -0800

    test sql database

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+
   return (
     <div className="App">
       <h1>
@@ -8,9 +13,17 @@ function App() {
       </h1>
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e)=>{
+          setMovieName(e.target.value)
+        }}/>
         <label>Review:</label>
-        <input type="text" name="review" />    
+        <input
+          type="text"
+          name="review" 
+          onChange={(e) => {
+          setReview(e.target.value);
+        }}
+        />    
 
         <button>Submit</button>    
       </div>
